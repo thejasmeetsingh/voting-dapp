2f0ce5caaebcc7b82de1b43fdafac020e7ceb127
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2024-04-05 15:42:48 +0530

    Updated add candidate design and added file input for logo

--- client/src/components/AddCandidate.jsx
+++ client/src/components/AddCandidate.jsx
@@ -20,6 +20,25 @@ export default function AddCandidate(params) {
       <form onSubmit={onFormSubmit} className="max-w-md mx-auto">
         <div className="relative z-0 w-full mb-5 group">
           <input
+            // value={name}
+            // onChange={(e) => {
+            //   setName(e.target.value);
+            // }}
+            type="file"
+            name="logo"
+            id="logo"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            required
+          />
+          <label
+            htmlFor="logo"
+            className="peer-focus:font-medium absolute text-sm text-white-500 dark:text-white-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Logo
+          </label>
+        </div>
+        <div className="relative z-0 w-full mb-5 group">
+          <input
             value={name}
             onChange={(e) => {
               setName(e.target.value);
